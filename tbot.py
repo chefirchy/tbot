8d0981a098c5533d43dcc278759b8bc8c5c16ab4
chefirchy <42928834+chefirchy@users.noreply.github.com>
2020-04-28 21:45:39 +0300

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -14,7 +14,7 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import webapp2
 
-TOKEN = 'YOUR_BOT_TOKEN_HERE'
+TOKEN = '994681852:AAFagzvW_RRJmbpcmEI7WHAUCnsDC1FqlVs'
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
