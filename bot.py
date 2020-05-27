633e4d1edc7c06993f42af970761ba0846e2bbff
chefirchy <42928834+chefirchy@users.noreply.github.com>
2020-05-27 18:50:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ bot = telebot.TeleBot("994681852:AAGdns6Oa4IWJvvC1x61HNERlzcjOoTNIdA")
 def handle_command_menu(message):
     bot.send_message(chat_id=message.chat.id,
                      text="✨hello")
-@bot.message_handler(commands=['Ж', 'ж'])
+@bot.message_handler(commands=['J', 'j'])
 def handle_command_menu(message):
     bot.send_message(chat_id=message.chat.id,
                      text="⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️⬜️"\n
