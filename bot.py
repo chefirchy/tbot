0edf5c7af11ba8cc4cfe9cdf53866c40fb2a5602
chefirchy <42928834+chefirchy@users.noreply.github.com>
2020-06-04 22:35:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,11 @@ def handle_command_menu(message):
                      text="1.фундаментальна система розв'язків\n"
                           "2.ранг оператора\n"
                           "3.арифметичний n-вимірний простір\n"
-                          "4.Оператор у векторному просторі\n")
+                          "4.лінійний оператор\n"
+                          "5.однорідна система лінійних рівнянь\n"
+                          "6.система лінійних рівнянь\n"
+                          "7.лінійні та векторні простори\n"
+                          "8.n-вимірний вектор\n")
 @bot.message_handler(commands=['1', 'o'])
 def handle_command_menu(message):
     bot.send_message(chat_id=message.chat.id,
@@ -56,12 +60,28 @@ def handle_command_menu(message):
 def handle_command_menu(message):
     bot.send_message(chat_id=message.chat.id,    
                      text="Арифметичним n-вимірним простором будемо називати множину усіх n-вимірних векторів, на якій визначено операції додавання векторів та множення векторів")
-@bot.message_handler(commands=['3', 'c'])
+@bot.message_handler(commands=['4', 'c'])
 def handle_command_menu(message):
     bot.send_message(chat_id=message.chat.id,    
                      text="Оператор у векторному просторі L - функція множиною визначення і множиною значень якої є простір L")     
-    
-
+@bot.message_handler(commands=['5', 'c'])
+def handle_command_menu(message):
+    bot.send_message(chat_id=message.chat.id,    
+                     text="Система лінійних рівнянь називається однорідною (СЛОР), якщо вільний член у кожному рівнянні дорівнює нулю")
+@bot.message_handler(commands=['6', 'c'])
+def handle_command_menu(message):
+    bot.send_message(chat_id=message.chat.id,    
+                     text="Рівняння з n невідомими х1,х2,…,хn називається лінійним, якщо його можна подати у вигляді:\n"
+                          "а1х1+а2х2+…+ аnхn= b , (1.1)\n"
+                          "де а1,а2,…,ап– коефіцієнти, b – вільний член рівняння (дійсні числа)")         
+@bot.message_handler(commands=['7', 'c'])
+def handle_command_menu(message):
+    bot.send_message(chat_id=message.chat.id,    
+                     text="Множина V елементів x, y, z,… називається лінійним, або векторним простором, якщо сума х+у довільних двох її елементів х, у і добуток αх кожного її елемента х на будь-яке число α теж належать множині V")
+    @bot.message_handler(commands=['8', 'c'])
+def handle_command_menu(message):
+    bot.send_message(chat_id=message.chat.id,    
+                     text="Будь-який вектор арифметичного простору будемо розуміти як упорядкувану н-ку дійсних чисел")
 # stickers
 @bot.message_handler(content_types=["sticker"])
 def echo_all(message):
