925f6e59eaa70e466f0189b24d80d754e9fdd25d
chefirchy <42928834+chefirchy@users.noreply.github.com>
2020-06-05 12:38:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,41 +39,41 @@ def start_message(message):
                           "8.n-вимірний вектор\n"
                           "9.ядро оператора(позначення, розмірність)\n", reply_markup=keyboard2)
 @bot.message_handler(content_types=['text'])
-def start_message(message):
+def o_message(message):
     if message.text == '1':
         bot.send_message(message.chat.id, text="Фундаментальною системою розв'язків системи неоднорідних рівнянь називається така лінійно незалежна сукупність її розв'язків, що всякий розв'язок даної системи є якоюсь лінійною комбінацією розв'язків цієї сукупності", reply_markup=keyboard2)
 @bot.message_handler(content_types=['text'])
-def start_message(message):
+def d_message(message):
     if message.text == '2':
         bot.send_message(message.chat.id, text="Розмірність області значень LnA називають рангом оператора А і позначають rang A(приметка: тут л н,а не логарифм)\n", reply_markup=keyboard2)
 @bot.message_handler(content_types=['text'])
-def start_message(message):
+def t_message(message):
     if message.text == '3':
         bot.send_message(message.chat.id, text="Арифметичним n-вимірним простором будемо називати множину усіх n-вимірних векторів, на якій визначено операції додавання векторів та множення векторів", reply_markup=keyboard2)
 @bot.message_handler(content_types=['text'])
-def start_message(message):
+def c_message(message):
     if message.text == '4':
         bot.send_message(message.chat.id, text="Оператор у векторному просторі L - функція множиною визначення і множиною значень якої є простір L", reply_markup=keyboard2)
 @bot.message_handler(content_types=['text'])
-def start_message(message):
+def p_message(message):
     if message.text == '5':
         bot.send_message(message.chat.id, text="Система лінійних рівнянь називається однорідною (СЛОР), якщо вільний член у кожному рівнянні дорівнює нулю", reply_markup=keyboard2)
 @bot.message_handler(content_types=['text'])
-def start_message(message):
+def s_message(message):
     if message.text == '6':
         bot.send_message(message.chat.id, text="Рівняння з n невідомими х1,х2,…,хn називається лінійним, якщо його можна подати у вигляді:\n"
                           "а1х1+а2х2+…+ аnхn= b , (1.1)\n"
                           "де а1,а2,…,ап– коефіцієнти, b – вільний член рівняння (дійсні числа)", reply_markup=keyboard2)        
 @bot.message_handler(content_types=['text'])
-def start_message(message):
+def se_message(message):
     if message.text == '7':
         bot.send_message(message.chat.id, text="Множина V елементів x, y, z,… називається лінійним, або векторним простором, якщо сума х+у довільних двох її елементів х, у і добуток αх кожного її елемента х на будь-яке число α теж належать множині V", reply_markup=keyboard2)        
 @bot.message_handler(content_types=['text'])
-def start_message(message):
+def v_message(message):
     if message.text == '':
         bot.send_message(message.chat.id, text="", reply_markup=keyboard2)        
 @bot.message_handler(content_types=['text'])
-def start_message(message):
+def d_message(message):
     if message.text == '9':
         bot.send_message(message.chat.id, text="Ядром лінійного оператора А простору Ln називається сукупність всіх векторів цього простору, які оператором А відображаються в нульовий вектор.\n"
                            "Ядро лінійного оператора позначається KerA.\n" 
